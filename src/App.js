861295fe1e91e9fa5139063749dce70ffee743b0
Samuel Njenga <snjenga704@gmail.com>
2022-03-18 13:04:48 +0300

    feat: add SimpleTransition.js, SimpleTransition.css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { useRef } from "react";
-
-import { loremIpsum } from "lorem-ipsum";
-
-import { Main } from "./useTransitions/NotificationHubStyles.js";
-import { MessageHub } from "./useTransitions/NotificationHub.js";
+import SimpleTransition from "./useTransitions/SimpleTransition";
 
 const App = () => {
-  const ref = useRef(null);
-
-  const handleClick = () => {
-    ref.current?.(loremIpsum());
-  };
 
   return (
     <div style={{ marginLeft: "70px", marginTop: "40px" }}>
-      <Main onClick={handleClick}>
-        Click here to create notifications
-        <MessageHub
-          children={(add) => {
-            ref.current = add;
-          }}
-        />
-      </Main>
+      <SimpleTransition />
     </div>
   );
 };
