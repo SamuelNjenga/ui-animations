56e867b747a156573a375eb4969a4883b98ef524
Samuel Njenga <snjenga704@gmail.com>
2022-03-21 11:14:20 +0300

    feat: add VerticalParallax.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import React, { useRef } from "react";
+import React from "react";
 
-import { Parallax } from "@react-spring/parallax";
-
-import Page from "./parallax/Page";
-
-import "./parallax/Page.css";
+import VerticalParallax from "./parallax/VerticalParallax";
 
 const App = () => {
-  const parallax = useRef(null);
-
-  const scroll = (to) => {
-    if (parallax.current) {
-      parallax.current.scrollTo(to);
-    }
-  };
-
   return (
-    <div style={{ background: "#dfdfdf" }}>
-      <Parallax className="container" ref={parallax} pages={3} horizontal>
-        <Page offset={0} gradient="pink" onClick={() => scroll(1)} />
-        <Page offset={1} gradient="teal" onClick={() => scroll(2)} />
-        <Page offset={2} gradient="tomato" onClick={() => scroll(0)} />
-      </Parallax>
+    <div>
+      <VerticalParallax />
     </div>
   );
 };
